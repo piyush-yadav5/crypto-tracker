8f1a15db654c289a52cf87097036c6076b5843ff
Piyush5.Yadav <piyush5.yadav@ril.com>
2022-12-13 14:04:36 +0530

    Pagination decimal bug fixed

--- src/Containers/CoinsTable.jsx
+++ src/Containers/CoinsTable.jsx
@@ -75,7 +75,7 @@ const CoinsTable = ({ filtered, loading }) => {
                 }
 
             </div>
-            <Pagination totalCount={(filtered.length / 10)} paginate={paginate} />
+            <Pagination totalCount={Math.floor(filtered.length / 10)} paginate={paginate} />
         </Container>
     )
 }
