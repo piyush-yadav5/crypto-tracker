1e8d087dfd6801ea81c4f2b1018a291ab72024d6
Piyush5.Yadav <piyush5.yadav@ril.com>
2022-12-13 13:53:28 +0530

    Footer Added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../Containers/Banner/Banner'
 import Search from '../Containers/Search'
+import Footer from '../Containers/Footer'
 
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <div >
     <Banner />
     <Search />
+    <Footer />
     </div>
   )
 }
